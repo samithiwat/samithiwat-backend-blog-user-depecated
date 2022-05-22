50ab33ee66f8138541c6e8bbd0ea6e1dd1225500
samithiwat <samithiwat2544@gmail.com>
2022-05-22 22:22:20 +0700

    Create user entity

---
+++ src/main/java/com/samithiwat/user/bloguser/entity/User.java
@@ -0,0 +1,41 @@
+package com.samithiwat.user.bloguser.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(unique = true)
+    private Long userId;
+    private String description;
+
+    public User() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
